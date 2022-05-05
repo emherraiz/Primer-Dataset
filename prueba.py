01fa5266884f794e624df84ab015596217050e70
Enrique Marina <eherraiz@myuax.com>
2022-05-06 01:15:35 +0200

    probando criterio

--- prueba.py
+++ prueba.py
@@ -1,9 +1,16 @@
+from math import ceil, floor
 import numpy as np
 
 
 a = [1, 3, 4, 5, 6, 7]
 b = [2]
-print(round(0.34))
+print(6.4 % 2)
+
+def par_true(numero):
+    if numero % 2 == 0:
+        return True
+    else:
+        return False
 
 
 def validar_resultados(lista, media, desviacion_tipica):
@@ -15,6 +22,27 @@ def validar_resultados(lista, media, desviacion_tipica):
 
     return lista, repetir
 
+# Criterio ASTM - E29, se podrían utilizar criterios más faciles
+def cifra_significativa(media, error_total):
+    if error_total < 1:
+        decimal = 0
+        redondeo = error_total
+        while floor(redondeo) == 0:
+            redondeo *= 10
+            decimal += 1
+
+        redondeo *= 10
+
+        if (redondeo*10) % 5 == 0:
+            if par_true(redondeo):
+                redondeo = ceil(redondeo)
+            else:
+                redondeo = floor(redondeo)
+
+print(cifra_significativa)
+
+
+
 def medidas_directas_error(lista, error_inicial = 0, resolucion = 0):
     # Si tuvieramos nuestro aparato presentara algún error inicial debemos quitarselo a todas nuestras mediciones
     if error_inicial != 0:
@@ -23,7 +51,8 @@ def medidas_directas_error(lista, error_inicial = 0, resolucion = 0):
 
     # Calculamos la media y la desviacion típica.
     # He utilizado numpy aunque existen más modulos que nos proporcionan estas funciones.
-    # También se pueden crear de una manera muy sencilla
+    # Se pueden crear funciones para la media y desviación de manera muy sencilla.
+    # Importo numpy para agilicidar el desarrollo del ejercicio
     media = np.mean(lista)
     desviacion_tipica = np.std(lista)
 
@@ -46,6 +75,7 @@ def medidas_directas_error(lista, error_inicial = 0, resolucion = 0):
                 while round(numero, decimal) != round(numero, decimal + 1):
                     decimal += 1
         if decimal != 0:
+            # Redondeamos para ajustar el sesgo producido entre int y float
             resolucion = round(pow(0.1, decimal), decimal)
 
     # Calculamos el error total
